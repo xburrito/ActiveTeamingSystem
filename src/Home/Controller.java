d809f8f91cebeb08e9bc783966def55045079f77
xBurrito <awxchang@gmail.com>
2020-05-08 17:07:28 -0400

    Created Login and Register Form

--- src/Home/Controller.java
+++ src/Home/Controller.java
@@ -1,4 +1,13 @@
 package Home;
 
 public class Controller {
+
+    @FXML
+    private void open_registration (MouseEvent event){
+        Parent fxml = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        content_area.getChildren().removeAll();
+        content_area.getChildren().setAll(fxml);
+    }
+
+
 }
---
+++ src/Home/RegisterController.java
@@ -0,0 +1,14 @@
+package Home;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class RegisterController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
+
+}

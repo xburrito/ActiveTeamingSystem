280abdfd4642aa2888a06f93796c457dd99bf3c8
ZGabriell <gavrielspinoza@gmail.com>
2020-05-08 23:05:58 -0400

    Added HomeController.java

---
+++ src/Home/HomeController.java
@@ -0,0 +1,19 @@
+package Home;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+    private MainController mainController;
+    @FXML private AnchorPane homeAnchorPane;
+
+    // gets mainController
+    public void injectMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    public AnchorPane getHomeContent(){
+        return homeAnchorPane;
+        //homeAnchorPane.toFront();
+    }
+}

8101cf8dbe8694764eb98f16eb1f3b205324c410
ZGabriell <gavrielspinoza@gmail.com>
2020-05-08 23:23:40 -0400

    Fixed some settings in Main.java

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -7,17 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+    private static Stage primaryStage;
+    public static Stage getStage() { return primaryStage; } //method for controller: returns the stage
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @SuppressWarnings("static-access")
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage; // set primary stage
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Active Teaming System");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+        this.primaryStage.setOnCloseRequest(e -> System.exit(0));
     }
 }

8b01a0b00e4b0986e8a5e09d284b1790d28668dd
ZGabriell <gavrielspinoza@gmail.com>
2020-05-10 11:36:50 -0400

    Register View: All fields in a completed form can now retrieved by the RegisterAnchorPaneController. Later those values can be used to import to a database or for verification.

--- src/Controller/RegisterAnchorPaneController.java
+++ src/Controller/RegisterAnchorPaneController.java
@@ -1,12 +1,10 @@
 package Controller;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDatePicker;
-import com.jfoenix.controls.JFXRadioButton;
-import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.TextField;
 
 public class RegisterAnchorPaneController {
     private MainAnchorPaneController mainAnchorPaneController;
@@ -14,7 +12,7 @@ public class RegisterAnchorPaneController {
     // buttons
     @FXML private JFXButton buttonGoBack;
     @FXML private JFXButton buttonSubmitForm;
-    ToggleGroup group = new ToggleGroup();
+    ToggleGroup radioGroup = new ToggleGroup();
     @FXML private JFXRadioButton radioButtonOU;
     @FXML private JFXRadioButton radioButtonVIP;
     @FXML private JFXRadioButton radioButtonNS;
@@ -24,8 +22,8 @@ public class RegisterAnchorPaneController {
     @FXML private JFXTextArea fieldUsername;
     @FXML private JFXTextArea fieldEmail;
     @FXML private JFXDatePicker datePickerDOB;
-    @FXML private JFXTextArea fieldPassword;
-    @FXML private JFXTextArea fieldPassConfirm;
+    @FXML private JFXPasswordField fieldPassword;
+    @FXML private JFXPasswordField fieldPassConfirm;
     @FXML private JFXTextArea fieldReference;
 
 
@@ -39,10 +37,10 @@ public class RegisterAnchorPaneController {
 
     @FXML private void initialize(){
         // add all radioButtons to a group
-        radioButtonOU.setToggleGroup(group);
-        radioButtonVIP.setToggleGroup(group);
-        radioButtonNS.setToggleGroup(group);
-        radioButtonNO.setToggleGroup(group);
+        radioButtonOU.setToggleGroup(radioGroup);
+        radioButtonVIP.setToggleGroup(radioGroup);
+        radioButtonNS.setToggleGroup(radioGroup);
+        radioButtonNO.setToggleGroup(radioGroup);
 
     }
 
@@ -50,6 +48,29 @@ public class RegisterAnchorPaneController {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         if (event.getSource() == buttonSubmitForm) {
+
+            // first check that none of the fields are empty.
+            if (fieldUsername.getText() != null && fieldEmail.getText() != null && fieldPassword != null && datePickerDOB.getValue() != null &&
+                    fieldPassConfirm != null && fieldReference != null && radioGroup.getSelectedToggle() != null) {
+
+                // get all fields
+                String username = fieldUsername.getText();
+                String email = fieldEmail.getText();
+                String dateOfBirth = datePickerDOB.getValue().toString();
+                String password = fieldPassword.getText();
+                String passConfirm = fieldPassConfirm.getText();
+                String referrer = fieldReference.getText();
+                String radio = radioGroup.getSelectedToggle().toString();
+
+                // for debuggin only: print all entered fields
+                System.out.println(username);
+                System.out.println(email);
+                System.out.println(dateOfBirth);
+                System.out.println(password);
+                System.out.println(passConfirm);
+                System.out.println(referrer);
+                System.out.println(radio);
+            }
             // SEND FORM "NOTIFICATION" TO SU
         }
         else if (event.getSource() == buttonGoBack) {
